1594b2d29b4d0d55c3d9ea47c1a210a4d9c87440
Ivan Panteliuk <i.v.pantelyuk@gmail.com>
2023-04-24 18:56:25 +0200

    Add HW code

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import {
+  Header,
+  Button,
+  SearchForm,
+  ButtonLabel,
+  SearchInput,
+} from './Searchbar.styled';
+
+class Searchbar extends Component {
+  state = { query: '' };
+
+  formSubmitHandler = evt => {
+    evt.preventDefault();
+
+    if (this.state.query.trim() === '') return;
+
+    this.props.onSubmit(this.state.query.trim());
+    this.resetInput();
+  };
+
+  resetInput() {
+    this.setState({ query: '' });
+  }
+
+  handleInputChange = evt => {
+    this.setState({ query: evt.currentTarget.value.toLowerCase() });
+  };
+
+  render() {
+    const { query } = this.state;
+    return (
+      <Header>
+        <SearchForm onSubmit={this.formSubmitHandler}>
+          <Button type="submit">
+            <ButtonLabel>Search</ButtonLabel>
+          </Button>
+
+          <SearchInput
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={query}
+            onChange={this.handleInputChange}
+          />
+        </SearchForm>
+      </Header>
+    );
+  }
+}
+
+Searchbar.propTypes = {
+  query: PropTypes.string.isRequired,
+};
+
+export default Searchbar;
